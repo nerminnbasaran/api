f9a4296bb4a9bef0ea4adc172d6f0ee2cad54be5
nnerminbasaran <n.nerminbasaran@gmail.com>
2023-04-16 17:23:34 +0300

    updating

--- src/test/java/base_urls/GmiBankBaseUrl.java
+++ src/test/java/base_urls/GmiBankBaseUrl.java
@@ -5,6 +5,8 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
+import static utils.AuthenticationGmiBank.gmiBankToken;
+
 public class GmiBankBaseUrl {
 
     protected RequestSpecification spec;
@@ -12,6 +14,10 @@ public class GmiBankBaseUrl {
     @Before
     public void setUp(){
 
-        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://www.gmibank.com").build();
+        spec = new RequestSpecBuilder().
+                addHeader("Authorization", "Bearer " + gmiBankToken()).
+                setContentType(ContentType.JSON).
+                setBaseUri("https://www.gmibank.com").
+                build();
     }
 }
